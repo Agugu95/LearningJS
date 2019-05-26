0f8ec88ddb0f07bcf2774acb1d27878550150da5
Agugu95 <fkrns1744@naver.com>
2019-05-26 19:10:33 +0900

    [6일차: 클로저를 통한 정보은닉]

---
+++ es6/closerScope2.js
@@ -0,0 +1,14 @@
+function hello(name) {
+    const _name = name;
+    return function () {
+        console.log(`Hello ${_name}`);
+    };
+}
+
+const hello1 = hello('');
+const hello2 = hello('closer');
+const hello3 = hello('memories scope');
+
+hello1('really'); // 객체의 호출
+hello2();
+hello3();
