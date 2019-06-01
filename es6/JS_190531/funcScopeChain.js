8cc2484b7a63fe2f3e2778671ac7dcfdd19bdcf1
Agugu95 <fkrns1744@naver.com>
2019-06-01 23:50:43 +0900

    10일차: 함수 스코프와 체인

---
+++ es6/JS_190531/funcScopeChain.js
@@ -0,0 +1,20 @@
+var a = 10;
+function funcScope1() {
+    var b = 20;
+    funcScope2();
+    function funcScope2() {
+        var c = 30;
+        console.log(a, b, c);
+    }
+}
+
+funcScope1();
+
+var i;
+for (i = 5; i >= 0; i--) {
+    ((i) => {
+        setTimeout(function () {
+            console.log(i === 0 ? "go" : i);
+        }, (5 - i) * 1000);
+    })(i);
+}
