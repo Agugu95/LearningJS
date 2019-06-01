50e2a860b447278adb5ee6e94efe787d451e996b
Agugu95 <fkrns1744@naver.com>
2019-06-01 23:50:32 +0900

    10일차: 함수 스코프

--- es6/JS_190531/funcScope.js
+++ es6/JS_190531/funcScope.js
@@ -1,6 +1,25 @@
-var i;
-for (i = 5; i >= 0; i--) {
-    setTimeout(() => {
-        console.log(i === 0 ? 'go' : i);
-    });
+// var i;
+// for (i = 5; i >= 0; i--) {
+//     setTimeout(() => {
+//         console.log(i === 0 ? 'go' : i);
+//     });
+// }
+
+function scopeOne() {
+    var i = 'first local';
+    console.log(i);
+    scopeTwo();
+    function scopeTwo() {
+        var i = 'second local';
+        return console.log(i);
+    }
 }
+
+scopeOne();
+var i = 'b';
+function A() {
+    i = 'a';
+    console.log(i);
+}
+
+A();
