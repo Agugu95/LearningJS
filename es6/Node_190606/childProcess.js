a9ed5f8b9c256b9564198902af6d6f1a7f17399f
Agugu95 <fkrns1744@naver.com>
2019-06-08 20:08:45 +0900

    자식 프로세스: 인코딩 변경해봤는데 안됨

--- es6/Node_190606/childProcess.js
+++ es6/Node_190606/childProcess.js
@@ -1,9 +1,9 @@
 // exec = 실행 fork = 복사 
 const { exec } = require('child_process');
 
-exec('dir', { encoding: 'euc-kr' }, (err, stdout, stderr) => {
+exec('dir', { encoding: 'utf8' }, (err, stdout, stderr) => {
     if (err) return console.error('Error executing "dir"');
-    this.stdout = stdout.toString(); // 버퍼를 문자열로 변환 
+    this.stdout = stdout.toString({ encoding: 'utf8' }); // 버퍼를 문자열로 변환 
     console.log(`StdOut: ${stdout}`);
     this.stderr = stderr.toString();
     if (stderr !== '') {
