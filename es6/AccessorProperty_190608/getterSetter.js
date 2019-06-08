5626ab0e8b12862853b0438ab04572bf1281f22a
Agugu95 <fkrns1744@naver.com>
2019-06-08 20:40:52 +0900

    [접근자프로퍼티: 메소드가 아닌 프로퍼티git add getterSetter.js!]

--- es6/AccessorProperty_190608/getterSetter.js
+++ es6/AccessorProperty_190608/getterSetter.js
@@ -1,6 +1,8 @@
 const USER_EMAIL = Symbol();
 // 프로퍼티에 심볼을 사용해서 직접 접근을 막음
-class Users { // 왜 안될까???
+class Users { // 왜 안될까?? 
+    // 접근자 프로퍼티는 말 그대로 함수를 
+    // 값으로 묶은 거 였음.. 프로퍼티로 접근해야함
     set email(value) {
         // @가 들어간 문자열은 모두 이메일이라고 생각하는 정규표현식
         if (!/@/.test(value)) throw new Error(`Invaild email: ${value}`);
@@ -13,5 +15,5 @@ class Users { // 왜 안될까???
 }
 
 const u = new Users();
-u.email('jane@doe.com');
-console.log(`User email: ${u.emali()}`);
+u.email = 'jane@doe.com';
+console.log(`User email: ${u.email}`);
